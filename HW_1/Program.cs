9b9abcb8d8200e057a2520d4ffb9c54bfac54bb5
Olga <olgakovl96@gmail.com>
2023-09-04 21:27:01 +0300

    programm done

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ShortStringsArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество строк в массиве: ");
+            int n = int.Parse(Console.ReadLine());
+
+            string[] inputArray = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Введите строку {i + 1}: ");
+                inputArray[i] = Console.ReadLine();
+            }
+
+            string[] shortStringsArray = FilterShortStrings(inputArray);
+
+            Console.WriteLine("Строки длиной до 3 символов:");
+            foreach (string str in shortStringsArray)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.ReadLine();
+        }
+
+        static string[] FilterShortStrings(string[] array)
+        {
+            int count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                string str = array[i];
+                if (str.Length <= 3)
+                {
+                    count++;
+                }
+            }
+
+            string[] resultArray = new string[count];
+            int index = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                string str = array[i];
+                if (str.Length <= 3)
+                {
+                    resultArray[index] = str;
+                    index++;
+                }
+            }
+
+            return resultArray;
+        }
+    }
+}
